2c434f6662236159f1fb6f4d1ccc970dc4aab019
Lain <1297441127@qq.com>
2022-12-25 18:39:37 +0800

    feat: update dep list

--- src/views/workloads/depList.vue
+++ src/views/workloads/depList.vue
@@ -8,15 +8,15 @@
       fit
       highlight-current-row
     >
-      <el-table-column align="center" label="ID" width="95">
+      <el-table-column align="center" label="ID" width="60">
         <template slot-scope="scope">
           {{ scope.$index+1 }}
         </template>
       </el-table-column>
       <el-table-column label="状态">
         <template slot-scope="scope">
-          {{ scope.row.is_completed }}
-          <p>{{ scope.row.message }}</p>
+          <p v-html="getStatus(scope.row)"></p>
+          <p class="is-red">{{ getMessage(scope.row) }}</p>
         </template>
       </el-table-column>
       <el-table-column label="名称">
@@ -31,7 +31,14 @@
       </el-table-column>
       <el-table-column label="副本数">
         <template slot-scope="scope">
-          {{ scope.row.replicas }}
+          <span>{{ scope.row.replicas[0] }}</span> /
+          <span class="is-green">{{ scope.row.replicas[1] }}</span> /
+          <span class="is-red">{{ scope.row.replicas[2] }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间">
+        <template slot-scope="scope">
+          {{ scope.row.created_at }}
         </template>
       </el-table-column>
 <!--      <el-table-column label="Author" width="110" align="center">
@@ -89,7 +96,24 @@ export default {
           this.$forceUpdate()
         }
       }
+    },
+    getStatus(row) {
+      if (row.is_completed) {
+        return '<span class="is-green">Active</span>'
+      }
+      return '<span class="is-red">Waiting</span>'
+    },
+    getMessage(row) {
+      if (!row.is_completed) {
+        return row.message
+      }
+      return ''
     }
   }
 }
 </script>
+
+<style>
+.is-red {color: red}
+.is-green {color: green}
+</style>
