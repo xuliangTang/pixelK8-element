7636fbda2d3f2ccdc108cd7a24aed2ec05082f55
xuliangTang <1297441127@qq.com>
2023-02-01 15:12:00 +0800

    fix: array input

--- src/components/Common/ArrayInput.vue
+++ src/components/Common/ArrayInput.vue
@@ -11,6 +11,11 @@ export default {
     }
   },
   watch: {
+    data: {
+      handler(newVal, oldVal) {
+        this.unParse()
+      }
+    },
     text: {
       handler(newVal, oldVal) {
         this.$emit('update:data', this.parseData())
@@ -18,21 +23,24 @@ export default {
     }
   },
   created() {
-    if (this.split !== undefined) {
-      this.strsplit = this.split
-    }
-
-    if (this.data !== undefined && this.data != null) {
-      // 如果数组里 某一项有空格 ，两头需要加单引号 或双引号
-      this.data.forEach((item, itemindex) => {
-        if (/\s+/.test(item)) {
-          this.data[itemindex] = "'" + item + "'"
-        }
-      })
-      this.text = this.data.join(this.strsplit)
-    }
+    this.unParse()
   },
   methods: {
+    unParse() {
+      if (this.split !== undefined) {
+        this.strsplit = this.split
+      }
+
+      if (this.data !== undefined && this.data != null) {
+        // 如果数组里 某一项有空格 ，两头需要加单引号 或双引号
+        this.data.forEach((item, itemindex) => {
+          if (/\s+/.test(item)) {
+            this.data[itemindex] = "'" + item + "'"
+          }
+        })
+        this.text = this.data.join(this.strsplit)
+      }
+    },
     parseData() {
       // 类似 echo -c 'echo The app is running! && sleep 3600'
       // 单引号和双引号 里面的内容 不做 split
@@ -42,13 +50,13 @@ export default {
       var mList = str.match(pattern)
       if (!mList || mList.length === 0) {
         str = str.replace(/\s+/g, ' ')
-        return str.split(' ')
+        return str.split(this.strsplit)
       }
       for (var i = 0; i < mList.length; i++) {
         str = str.replace(mList[i], '_#_' + i) // _#_1  _#_2  _#_3
       }
       str = str.replace(/\s+/g, ' ') // 多个空格要变成一个 空格
-      var ret = str.split(' ') // 数组
+      var ret = str.split(this.strsplit) // 数组
       var p = /^_#_(\d+)$/
       ret.forEach((item, index) => {
         if (p.test(item)) {
